d94a4645916556ab152c14874983193876c13a6f
Henry Withoot <osc2nuke@gmail.com>
2018-07-27 14:54:05 +0200

    Fix collapsing

--- admin/includes/column_left.php
+++ admin/includes/column_left.php
@@ -52,33 +52,26 @@
     }	
 	$adminAppMenu = '';	
 	$adminAppMenu .= '<nav id="adminAppMenu" class="col-md-2 d-none d-md-block bg-light sidebar">';
-	$adminAppMenu .= '<div class="sidebar-sticky" id="accordion" aria-multiselectable="true">';
+	$adminAppMenu .= '<div class="sidebar-sticky" id="columnLeft">';
 	
 	$counter = 0;
 
 	foreach ($cl_box_groups as $groups) {
-	//$adminAppMenu .= '<div class="card">';	
     
-        //$adminAppMenu .= '<div class="card-header" id="collapseListGroupHeading'.$counter.'">';
         $adminAppMenu .= '  <h6 id="collapseListGroupHeading'.$counter.'" class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">';
-        $adminAppMenu .= '    <a class="d-flex align-items-center text-muted" data-toggle="collapse" data-parent="#accordion" href="#collapseListGroup'.$counter.'" aria-expanded="false" aria-controls="collapseListGroup'.$counter.'"">';
+        $adminAppMenu .= '    <a class="d-flex align-items-center text-muted" data-toggle="collapse"  data-target="#collapseListGroup'.$counter.'" aria-expanded="false" aria-controls="collapseListGroup'.$counter.'"">';
         $adminAppMenu .= '      <span data-feather="plus-circle"></span> <span class="ml-1">' . $groups['heading'] . '</span>';
         $adminAppMenu .= '    </a>';
         $adminAppMenu .= '  </h6>';
-        //$adminAppMenu .= '</div>';        
         
-        //$adminAppMenu .= '<div id="collapseListGroup'.$counter.'" class="card-collapse collapse" aria-labelledby="collapseListGroupHeading'.$counter.'">';
-		$adminAppMenu .= '<div id="collapseListGroup'.$counter.'" class="card-collapse collapse" role="tabpanel" aria-labelledby="collapseListGroupHeading'.$counter.'">';
-        //$adminAppMenu .= '  <div class="card-body">';
+		$adminAppMenu .= '<div id="collapseListGroup'.$counter.'" class="card-collapse collapse" data-parent="#columnLeft" role="tabpanel" aria-labelledby="collapseListGroupHeading'.$counter.'">';
 		$adminAppMenu .= '      <ul class="nav flex-column mb-2">';
 			foreach ($groups['apps'] as $app) {               
                 $adminAppMenu .= '<li class="nav-item"><a class="nav-link" href="' . $app['link'] . '">' . $app['title'] . '</a></li>';
 			}
 		$adminAppMenu .= '      </ul>';
-		//$adminAppMenu .= '  </div>';
 		$adminAppMenu .= '</div>';
 		
-		//$adminAppMenu .= '</div>';//end card
 		$counter++;    
 	}
 	
