c3ade3dcdcb85339ade915505341f46e9f390189
Henry Withoot <osc2nuke@gmail.com>
2018-07-28 19:58:22 +0200

    Fix class call

--- admin/modules_admin.php
+++ admin/modules_admin.php
@@ -142,10 +142,10 @@
   $installed_modules = array();
   for ($i=0, $n=sizeof($directory_array); $i<$n; $i++) {
     $file = $directory_array[$i];
-
-    include($module_language_directory . $language . '/modules/' . $module_type . '/' . $file);
-    include($module_directory . $file);
-
+    if (isset($_GET['list'])) {
+      include($module_language_directory . $language . '/modules/' . $module_type . '/' . $file);
+      include($module_directory . $file);
+    }
     $class = substr($file, 0, strrpos($file, '.'));
     if (tep_class_exists($class)) {
       $module = new $class;
